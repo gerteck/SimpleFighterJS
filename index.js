23539d402a45a342fb2e3376e73ebab8cd366a19
gerteck <changerteck@gmail.com>
2024-06-01 00:08:31 +0800

    Project Setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+// c stands for context. Shortened for convenience.
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+
+
+
+
+
+
+
+
+
